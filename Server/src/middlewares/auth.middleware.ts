7791560c8e5543d4f73af0e0a73e624820491031
satwik <mohanty.satwik290@gmail.com>
2026-02-08 20:52:40 +0530

    add auth middleware  to protect  routes

---
+++ Server/src/middlewares/auth.middleware.ts
@@ -0,0 +1,17 @@
+import { env } from "../config/env";
+import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from "express";
+
+const authmiddleware = (req: Request, res: Response, next: NextFunction)=>{
+    const token = req.cookies.token || req.header('Authorization')?.replace('Bearer ', '');
+    if(!token){
+        return res.status(401).json({success: false , message : 'Unauthorized'});
+    }
+    try {
+    const decodeed = jwt.verify(token , env.JWT_SECRET) as {id : string};
+    (req as any).user = decodeed.id;
+    next();
+    } catch (error) {
+        return res.status(401).json({ success: false, message: 'Token is invalid or expired' });
+    }
+};
